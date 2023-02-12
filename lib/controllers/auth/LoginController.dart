55fead8e041d442dc6facff8b203f897801d5562
Sohail Afzal <sohail.afzal@barqaab.com>
2023-02-12 16:57:34 +0500

    12-02-2022

--- lib/controllers/auth/LoginController.dart
+++ lib/controllers/auth/LoginController.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
+import 'package:first_project/modal/UserModal.dart';
 
 class LoginController extends GetxController {
   final GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
@@ -63,33 +64,27 @@ class LoginController extends GetxController {
     }
     loginFormKey.currentState!.save();
 
-    var headers = {'Content-Type': 'application/json'};
+    var headers = {'content-Type': 'application/json'};
     try {
       var url = Uri.parse(BaseAPI.baseURL + EndPoints.login);
-      print(url);
       Map body = {
         'email': emailController.text.trim(),
         'password': passwordController.text
       };
       http.Response response =
           await http.post(url, body: jsonEncode(body), headers: headers);
-      print(response.body.toString());
-      if (response.statusCode == 200) {
-        final json = jsonDecode(response.body);
-        if (json['code'] == 0) {
-          var token = json['data']['Token'];
-          final SharedPreferences? prefs = await _prefs;
-          await prefs?.setString('token', token);
+      final json = jsonDecode(response.body);
+      if (json['status'] == 200) {
+        var token = json['data']['Token'];
+        final SharedPreferences? prefs = await _prefs;
+        await prefs?.setString('token', token);
 
-          emailController.clear();
-          passwordController.clear();
-          print(response.body);
-          //Get.off(HomeScreen());
-        } else if (json['code'] == 1) {
-          throw jsonDecode(response.body)['message'] ?? "Time oute";
-        }
+        emailController.clear();
+        passwordController.clear();
+
+        //Get.off(HomeScreen());
       } else {
-        throw jsonDecode(response.body)["Message"] ?? "Unknown Error Occured";
+        throw jsonDecode(response.body)["message"] ?? "Unknown Error Occured";
       }
     } catch (error) {
       Get.back();
