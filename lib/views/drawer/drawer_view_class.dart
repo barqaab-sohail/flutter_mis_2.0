ec1eb6a1a4733b947c6da942a5c919f72dadd885
Sohail Afzal <47312385+barqaab-sohail@users.noreply.github.com>
2023-04-15 16:36:15 +0500

    15-04-2023

--- lib/views/drawer/drawer_view_class.dart
+++ lib/views/drawer/drawer_view_class.dart
@@ -126,7 +126,7 @@ class DrawerViewClass {
     if (menuItem.level == 1) {
       lp = 10;
       fontSize = 14;
-      iconColor = Colors.red;
+      iconColor = Colors.green;
     }
     if (menuItem.level == 2) {
       lp = 20;
